cdbb9cd62c6b057d746f223ff544f4e936023be2
uzma shaik <uzma97910@gmail.com>
2025-03-04 15:46:18 +0100

    feat(routes): add portfolio routes for CRUD operations

---
+++ routes/portfolio.routes.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+//POST /portfolios
+
+router.post("/portfolios", (req, res, next) => {
+  const { userId, title, about, projects, experience, skills } = req.body();
+  const newPortfolio = {
+    userId,
+    title,
+    about,
+    projects,
+    experience,
+    skills,
+  };
+  Portfolio.create(newPortfolio)
+    .then((response) => {
+      res.status(200).json({ data: response });
+    })
+    .catch((err) => {
+      res.status(500).json({ Error: err });
+    });
+});
+
+//GET /portfolios
+
+router.get("/portfolios", (req, res, next) => {
+  Portfolio.find()
+    .then((allPortfoliosFromDb) => {
+      res.status(200).json(allPortfoliosFromDb);
+    })
+    .catch((err) => {
+      res.status(500).json({ Error: err });
+    });
+});
+
+//Get one Portfolio
+
+router.get("/portfolios/:portfolioId", (req, res, next) => {
+  const { portfolioId } = req.params();
+
+  Portfolio.findById(portfolioId)
+    .then((portfolioFromDb) => {
+      res.status(200).json(portfolioFromDb);
+    })
+    .catch((err) => {
+      res.status(500).json({ Error: err });
+    });
+});
+
+//PUT /portfolios/:portfolioId
+
+router.put("/portfolios/:portfolioId", (req, res, next) => {
+  const { portfolioId } = req.params();
+
+  if (!mongoose.Types.ObjectId.isValid(portfolioId)) {
+    res.status(400).json({ message: "Specified ID is not valid" });
+  }
+
+  Portfolio.findByIdAndUpdate(portfolioId, req.body, { new: true })
+    .then((updatedPortfolio) => {
+      res.status(201).json(updatedPortfolio);
+    })
+    .catch((err) => {
+      res.status(500).json({ Error: err });
+    });
+});
+
+//DELETE
+
+router.delete("/portfolio", (req, res, next) => {
+  const { portfolioId } = req.params();
+  if (!mongoose.Types.ObjectId.isValid(portfolioId)) {
+    res.status(400).json({ message: "Specified ID is not valid" });
+  }
+
+  Portfolio.findByIdAndRemove(portfolioId)
+    .then(() => {
+      res.status(200).json({ message: "Portfolio deleted successfully!" });
+    })
+    .catch((err) => {
+      res.status(500).json({ Error: err });
+    });
+});
+
+module.exports = router;
